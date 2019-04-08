e491092a33b9b3a3852ab9b7f9117ae3a1a9d798
CKeogh <c.keogh.10.ck@gmail.com>
2019-04-08 11:17:21 +0100

    update github link in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = (props) => {
     return <footer className="footer">
-        <a className="githubLink" href="https://github.com/CKeogh/nc-news-front-end"><i className="fab fa-github"></i></a>
+        <a className="githubLink" href="https://github.com/CKeogh/NC-News-FE"><i className="fab fa-github"></i></a>
     </footer>
 }
 
